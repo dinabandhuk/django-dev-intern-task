0027f79b6da96a1f5e1ff07033c3a708458cdd35
Dinabandhu Khatiwada <dinabandhukhatiwada@gmail.com>
2025-02-18 18:32:49 +0545

    set DEBUG=False for production deployment

--- studentEntry/studentEntry/settings.py
+++ studentEntry/studentEntry/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zvqlwyd9kl=7fl368-ebmf76)8znc*@1v_ovuf88(c+t!()ge#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
