091cd097610248a42892809cd2da3fee0be57f54
Dinabandhu Khatiwada <dinabandhukhatiwada@gmail.com>
2025-02-18 15:47:04 +0545

    define the Student model with appropriate fields

--- studentEntry/entrySystem/models.py
+++ studentEntry/entrySystem/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    Name = models.CharField(max_length=200)
+    Age = models.IntegerField()
+    Address = models.CharField(max_length=300)
+    Grade = models.CharField(max_length=1)
+    Major = models.CharField(max_length=100)
+    
