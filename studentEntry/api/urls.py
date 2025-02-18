ce2bbff11995b79a5e64fbbdfde4df81df83f11a
Dinabandhu Khatiwada <dinabandhukhatiwada@gmail.com>
2025-02-18 20:11:38 +0545

    add api endpoints

--- studentEntry/api/urls.py
+++ studentEntry/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet
 
 router = DefaultRouter()
-router.register(r'students', StudentViewSet)
+router.register(r'students', StudentViewSet, basename="student")
 
 urlpatterns = [
     path('', include(router.urls)),
