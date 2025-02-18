4b01cc064280c61f8dfcbe3e75f4b02a9190208c
Dinabandhu Khatiwada <dinabandhukhatiwada@gmail.com>
2025-02-18 15:48:14 +0545

    define views

--- studentEntry/entrySystem/views.py
+++ studentEntry/entrySystem/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def helloworld(request):
+    return HttpResponse("Hello form django!")
