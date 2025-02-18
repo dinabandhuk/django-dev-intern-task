247e2be10b392445d88669b5fe7be8579fe46180
Dinabandhu Khatiwada <dinabandhukhatiwada@gmail.com>
2025-02-18 18:28:23 +0545

    add student methods

--- studentEntry/entrySystem/urls.py
+++ studentEntry/entrySystem/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from .views import student_list, student_create, student_delete, student_update, student_detail
 
 urlpatterns = [
-    path("", views.helloworld, name="helloworld")
+    path('list/', student_list, name='student_list'),
+    path('new/', student_create, name='student_create'),
+    path('update/<int:id>/', student_update, name='student_update'),
+    path('delete/<int:id>/', student_delete, name='student_delete'),
+    path('student/<int:id>/', student_detail, name = 'student_detail')
 ]
+
