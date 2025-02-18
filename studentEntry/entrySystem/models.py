4c97d8d0d3cb76a57a668acf007b0cfc86cc0987
Dinabandhu Khatiwada <dinabandhukhatiwada@gmail.com>
2025-02-18 18:27:27 +0545

    modify models to display object names

--- studentEntry/entrySystem/models.py
+++ studentEntry/entrySystem/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    Name = models.CharField(max_length=200)
-    Age = models.IntegerField()
-    Address = models.CharField(max_length=300)
-    Grade = models.CharField(max_length=1)
-    Major = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    age = models.PositiveIntegerField()
+    address = models.TextField()
+    grade = models.CharField(max_length=1)
+    major = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
